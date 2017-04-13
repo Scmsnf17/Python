1f22c93c57eeebec4459dec46bcb7d67468ab2d0
silascastro <silascastro15@gmail.com>
2017-04-13 00:43:49 -0400

    Add files via upload

---
+++ exercicios operações.py
@@ -0,0 +1,9 @@
+#calcular notas bimestrais
+
+print("vamos calcular as notas bimestrais!")
+Bim_1 = float(input("Digite sua nota: "))
+Bim_2 = float(input("Digite sua nota: "))
+Bim_3 = float(input("Digite sua nota: "))
+Bim_4 = float(input("Digite sua nota: "))
+
+print('A média de suas notas é:',(Bim_1+Bim_2+Bim_3+Bim_4)/4)
