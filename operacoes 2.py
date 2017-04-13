46bfde6817342b81b3b5d638a9db503ba86b5b80
silascastro <silascastro15@gmail.com>
2017-04-13 00:45:48 -0400

    Add files via upload

---
+++ operacoes 2.py
@@ -0,0 +1,7 @@
+#calcular horas trabalhadas
+
+print("Horas Trabalhadas!")
+T = float(input("Quanto você ganha por hora: "))
+H = int(input("Quantos horas você trabalhou: "))
+
+print('o seu salário mensal é: ',(T*H))
