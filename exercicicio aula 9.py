d5a98d24330118e0b893aa5790039b3f32c68018
silascastro <silascastro15@gmail.com>
2017-04-13 00:46:39 -0400

    Add files via upload

---
+++ exercicicio aula 9.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Você pode beber")
+    if idade >= 21:
+        print("Voce é vip")
+
+else:
+    print("você só pode beber refrigerante")
